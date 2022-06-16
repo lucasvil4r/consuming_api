00a364ad1c2f6b0254b273ca37db5d6f67aa63d4
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-06-16 17:44:01 -0300

    APIs

---
+++ Place Details/PlaceDetails.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key=YOUR_API_KEY"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
