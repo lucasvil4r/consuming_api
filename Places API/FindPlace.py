ebb04dc19e2339978395cdfacc627ba92e311451
Richard Quintana <63418880+richard-gpcabling@users.noreply.github.com>
2022-06-18 00:41:40 -0300

    Update FindPlace.py
    Lucas, tirei a chave do código. O Google me reportou que ela estava exposta em um repositório público, e que não era seguro

--- Places API/FindPlace.py
+++ Places API/FindPlace.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=cabeamento%20estruturado&inputtype=textquery&fields=formatted_phone_number%2name%2geometry&key=AIzaSyABEQ7SN-jsgy_YnQDOizet_VK0xFSwJ_w"
+url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=cabeamento%20estruturado&inputtype=textquery&fields=formatted_phone_number%2name%2geometry&key=[GoogleApiKey]"
 
 payload= {}
 headers = {}
